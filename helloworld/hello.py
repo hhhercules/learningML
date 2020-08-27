15e1c2c7d1010168ab6f3cec52596f8103227014
hhhercules <34164708+hhhercules@users.noreply.github.com>
2020-08-27 19:00:18 +0800

    perceptron-getdata

--- helloworld/hello.py
+++
@@ -1,7 +0,0 @@
-print("hello world!")
-print("step2")
-print("step3")
-print("step2")
-print("step3")
-print("ss")
-#11111
---
+++ perceptron/perceptron.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt 
+#load data
+iris = load_iris()
+print(type(iris))
+df = pd.DataFrame(iris.data, columns = iris.feature_names)
+df['label'] = iris.target 
+df.columns = [
+    'sepal length','sepal width','petal length','petal width','label'
+]
+df.label.value_counts()
+plt.scatter(df[:50]['sepal length'],df[:50]['sepal width'],label = '0')
+plt.scatter(df[50:100]['sepal length'],df[50:100]['sepal width'], label = '1')
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+plt.show()
+data = np.array(df.iloc[:100, [9, 1, -1]])
+X, y = data[:,:,-1],data[:,-1]
+y = np.array([1 if i == 1 else -1 for i in y])
